023b63b16b2221ee3086a25ad242df6347296cde
ArastooYsf <robowater3@gmail.com>
2025-04-28 22:19:00 +0330

    Added View Models (Not Complete)

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MedSync.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    [NotifyPropertyChangedFor()]
+    private bool _sideMenuExpanded = true;
+    
+    public  
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MedSync.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+    
+}
