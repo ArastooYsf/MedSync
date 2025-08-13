8ef28b07b9d280e59109be21ddba4e0d689d86e1
ArastooYsf <robowater3@gmail.com>
2025-08-13 23:39:03 +0330

    banana

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Svg.Skia;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MedSync.ViewModels;
+
+public partial class MainViewModel :ViewModelBase
+{
+    [ObservableProperty]
+    private bool _sidePanelExpanded = false;
+
+    [RelayCommand]
+    private void SidePanelResize()
+    {
+        SidePanelExpanded = !SidePanelExpanded;
+    }
+    
+}
