f1d1421ae145e5f4c7d3ed8d4b13ffad87380872
ArastooYsf <robowater3@gmail.com>
2026-09-04 23:26:38 +0330

    add IsCompleted column to Appointments table

---
+++ Migrations/20260705144744_AddIsCompletedToAppointment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedSync.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsCompletedToAppointment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "Appointments",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "Appointments");
+        }
+    }
+}
